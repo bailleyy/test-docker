409c7c8d5ccd4ebe95e2c7d76073a69b33b880ea
b_aiIeyy <discordbailey12@gmail.com>
2024-12-02 16:13:48 +0000

    HELLO

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ server.disable("x-powered-by");
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+server.get("/", (req, res) => {
+  return res.status(200).json({ success: true, message: "ALIVE" })
+})
+
 server.listen(90, async () => {
   console.log(`[BOOT]: Server is running on port 90.`);
 });
